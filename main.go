27282cb4aa436b1f2b2166c04b35a7b4af759e34
Damien Nelson <dhnisaman@gmail.com>
2025-03-05 21:23:59 -0500

    change none file served endpoints to api

--- main.go
+++ main.go
@@ -27,9 +27,9 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepath)))))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", cfg.handlerHits)
-	mux.HandleFunc("POST /reset", cfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", cfg.handlerHits)
+	mux.HandleFunc("POST /api/reset", cfg.handlerReset)
 
 	server := &http.Server{
 		Handler: mux,
