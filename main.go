9afea636c50df50a35fa1878cf048b7fd4e68faf
Damien Nelson <dhnisaman@gmail.com>
2025-03-08 01:45:34 -0500

    connect db

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/DNelson35/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
 	port := "8080"
 	filepath := "."
+	
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		fmt.Println("failed to connect")
+	}
+	dbQueries := database.New(db)
 
 	var cfg apiConfig
+	cfg.db = dbQueries
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepath)))))
