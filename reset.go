1e7b8c534a54fc780307ae8ec789fc880cb01541
Damien Nelson <dhnisaman@gmail.com>
2025-03-09 00:32:58 -0500

    update reset to delete all users and limit to dev only

--- reset.go
+++ reset.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func(cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	dev := os.Getenv("PLATFORM")
+	if dev != "dev" {
+		sendError(w, 403, "Forbidden")
+		return
+	}
+	err := cfg.db.DeleteUsers(r.Context())
+	if err != nil {
+		sendError(w, 500, "failed to delete users")
+		return
+	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileServerHits.Store(0)
