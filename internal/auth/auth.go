c88a19342a9def8214c84e7fdd1a8255c1fc7640
Damien Nelson <dhnisaman@gmail.com>
2025-03-12 23:58:01 -0400

    add methods for creating jwt token an validating

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
- import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error){
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -16,4 +23,39 @@ func CheckPassword(password, hash string) error {
 		return err
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: fmt.Sprintf("%v", userID),
+	})
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error){
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	if !token.Valid {
+		return uuid.UUID{}, fmt.Errorf("invalid token")
+	}
+	userID, err := uuid.Parse(claims.Subject)
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return userID, nil
 }
