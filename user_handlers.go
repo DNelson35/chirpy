2ecdbe8208a8978e962a54d80a6db876ce916825
Damien Nelson <dhnisaman@gmail.com>
2025-03-18 01:20:37 -0400

    add polka webhook

--- user_handlers.go
+++ user_handlers.go
@@ -155,29 +155,3 @@ func(cfg *apiConfig) handleUpdateUser(w http.ResponseWriter, r *http.Request){
 
 	sendOK(w, 200, &resp)
 }
-
-func(cfg *apiConfig) handlePolkaWebHook(w http.ResponseWriter, r *http.Request){
-	type hookResp struct {
-		Event string `json:"event"`
-		Data struct {
-			UserID uuid.UUID `json:"user_id"`
-		} `json:"data"`
-	}
-	var req hookResp
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&req); err != nil {
-		sendError(w, 400, "bad request")
-	}
-	if req.Event != "user.upgraded"{
-		w.WriteHeader(204)
-		return
-	}
-	err := cfg.db.UpgradeUserChirpyRed(r.Context(), req.Data.UserID)
-	if err != nil {
-		w.WriteHeader(404)
-		return
-	}
-
-	w.WriteHeader(204)
-}
-
